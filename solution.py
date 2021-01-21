cb04063b7e8be82e31af126aa85873e8b13b0b65
Hajaniaina Andrianavalona <hajaniaina.andri@gmail.com>
2021-01-21 21:29:53 +0300

    refactor: Add more option on city choice, add city's initial name
    Add only initial to get the name of the city to avoid typing error

--- solution.py
+++ solution.py
@@ -4,9 +4,12 @@ import numpy as np
 
 # Dictionary for Country Name and Data Filtering
 CITY_DATA = {'chicago': 'chicago.csv',
+             'c': 'chicago.csv',
 			 'new york': 'new_york_city.csv',
 			 'new york city': 'new_york_city.csv',
-			 'washington': 'washington.csv'}
+             'n': 'new_york_city.csv',
+			 'washington': 'washington.csv',
+             'w': 'washington.csv'}
 months = ["january", "february", "march", "april", "may", "june"]
 
 def get_filters():
